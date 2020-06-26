8dcc616685d681adcb90a38e09b79e3629312c71
europani <48157259+europani@users.noreply.github.com>
2020-06-26 16:55:25 +0900

    Delete 조인.sql

--- academy/SQL/조인.sql
+++
@@ -1,9 +0,0 @@
-SELECT e.ename, d.dname
-FROM emp e, dept d;
-
-SELECT * FROM student, department;
-
--- equi join --
-SELECT s.NAME, s.deptno1, d.dname
-FROM student s, department d
-WHERE s.deptno1 = d.deptno;
